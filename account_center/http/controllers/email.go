e8ea22e83d04094c0d3ec2614ed647a864e37d1d
qgymje <chaingaoming86@gmail.com>
2016-09-20 18:17:47 +0800

    update user controller add sms/email register handler

---
+++ account_center/http/controllers/email.go
@@ -0,0 +1,18 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+// Email sms controlelr
+type Email struct {
+	Base
+}
+
+// RegisterCode create a register code
+func (e *Email) RegisterCode(c *gin.Context) {
+
+}
+
+// VerifyRegisterCode verify register code
+func (e *Email) VerifyRegisterCode(c *gin.Context) {
+
+}
--- account_center/http/controllers/user.go
+++ account_center/http/controllers/user.go
@@ -1,32 +1,20 @@
 package controllers
 
 import (
-	"log"
 	"net/http"
 	"platform/commons/codes"
 	"platform/commons/grpc_clients/user"
 	pb "platform/commons/protos/user"
-	"platform/utils"
 
 	"github.com/gin-gonic/gin"
 )
 
+// User represent user actions
 type User struct {
 	Base
-	userRPCAddress string
 }
 
-func (u *User) getUserRPCAddress() string {
-	if u.userRPCAddress != "" {
-		return u.userRPCAddress
-	}
-
-	host := utils.GetConf().GetString("app.rpc_host")
-	port := utils.GetConf().GetString("app.rpc_port")
-	u.userRPCAddress = host + port
-	return u.userRPCAddress
-}
-
+// Auth verify token
 func (u *User) Auth(c *gin.Context) {
 	token, errorCode := u.getToken(c)
 	if errorCode != codes.ErrorCodeSuccess {
@@ -36,18 +24,19 @@ func (u *User) Auth(c *gin.Context) {
 	}
 	pbToken := pb.Token{Token: token}
 	auth := userClient.NewUser(u.getUserRPCAddress())
-	if reply, err := auth.Auth(&pbToken); err != nil {
+	var reply *pb.UserInfo
+	var err error
+	if reply, err = auth.Auth(&pbToken); err != nil {
 		respformat := u.Response(c, rpcErrorFormat(err.Error()), nil)
 		c.JSON(http.StatusOK, respformat)
 		return
-	} else {
-		respformat := u.Response(c, codes.ErrorCodeSuccess, reply)
-		c.JSON(http.StatusOK, respformat)
-		return
 	}
+	respformat := u.Response(c, codes.ErrorCodeSuccess, reply)
+	c.JSON(http.StatusOK, respformat)
+	return
 }
 
-// Register 表示用户注册操作
+// Register  register action
 func (u *User) Register(c *gin.Context) {
 	form, err := NewRegisterBinding(c)
 	if err != nil {
@@ -68,7 +57,7 @@ func (u *User) Register(c *gin.Context) {
 	return
 }
 
-// Login 表示用户登录操作
+// Login user login
 func (u *User) Login(c *gin.Context) {
 	form, err := NewLoginBinding(c)
 	if err != nil {
@@ -78,18 +67,18 @@ func (u *User) Login(c *gin.Context) {
 	}
 
 	ul := userClient.NewUser(u.getUserRPCAddress())
-	if reply, err := ul.Login(form.Config()); err != nil {
+	var reply *pb.UserInfo
+	if reply, err = ul.Login(form.Config()); err != nil {
 		respformat := u.Response(c, rpcErrorFormat(err.Error()), nil)
 		c.JSON(http.StatusOK, respformat)
 		return
-	} else {
-		respformat := u.Response(c, codes.ErrorCodeSuccess, reply)
-		c.JSON(http.StatusOK, respformat)
-		return
 	}
+	respformat := u.Response(c, codes.ErrorCodeSuccess, reply)
+	c.JSON(http.StatusOK, respformat)
+	return
 }
 
-// Logout 表示用户登出操作
+// Logout  user logout
 func (u *User) Logout(c *gin.Context) {
 	token, errorCode := u.getToken(c)
 	if errorCode != codes.ErrorCodeSuccess {
@@ -103,25 +92,26 @@ func (u *User) Logout(c *gin.Context) {
 		respformat := u.Response(c, rpcErrorFormat(err.Error()), nil)
 		c.JSON(http.StatusOK, respformat)
 		return
-	} else {
-		respformat := u.Response(c, codes.ErrorCodeSuccess, nil)
-		c.JSON(http.StatusOK, respformat)
-		return
 	}
+
+	respformat := u.Response(c, codes.ErrorCodeSuccess, nil)
+	c.JSON(http.StatusOK, respformat)
+	return
 }
 
+// Info user info
 func (u *User) Info(c *gin.Context) {
-	log.Println(u.apiVersion(c))
 	userID := u.GetUserID(c)
 	info := userClient.NewUser(u.getUserRPCAddress())
-	if reply, err := info.Info(&pb.UserID{UserID: userID}); err != nil {
+	var reply *pb.UserInfo
+	var err error
+	if reply, err = info.Info(&pb.UserID{UserID: userID}); err != nil {
 		respformat := u.Response(c, rpcErrorFormat(err.Error()), nil)
 		c.JSON(http.StatusOK, respformat)
 		return
-	} else {
-		u.RemovePBUserInfoToken(reply)
-		respformat := u.Response(c, codes.ErrorCodeSuccess, reply)
-		c.JSON(http.StatusOK, respformat)
-		return
 	}
+	u.RemovePBUserInfoToken(reply)
+	respformat := u.Response(c, codes.ErrorCodeSuccess, reply)
+	c.JSON(http.StatusOK, respformat)
+	return
 }
