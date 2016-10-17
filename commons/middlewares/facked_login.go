81ee60319b5e5e6ed2b4e473f79d66ccc71f5f92
qgymje <chaingaoming86@gmail.com>
2016-10-17 10:41:53 +0800

    fackedLogin middlewares update, add a default token if it is empty in dev mode

--- commons/middlewares/facked_login.go
+++ commons/middlewares/facked_login.go
@@ -2,12 +2,19 @@ package middlewares
 
 import "github.com/gin-gonic/gin"
 
-// user 1
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE0Nzg4NDQ2MjcsImlkIjoiNTdlMjI2N2VjODZhYjQ1YWYzZDE0ODA2In0.QEfA_RFWmSGpDU1bT_Gzdh0Jl06ft8oXBNntB5l3CEU"
+const (
+	authKey     = "Authorization"
+	tokenPrefix = "bearer "
+	token       = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE0Nzg4NDQ2MjcsImlkIjoiNTdlMjI2N2VjODZhYjQ1YWYzZDE0ODA2In0.QEfA_RFWmSGpDU1bT_Gzdh0Jl06ft8oXBNntB5l3CEU"
+)
 
+// FakedLogin add a facked user token to the header
 func FakedLogin() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Request.Header.Add("Authorization", "bearer "+token)
+		authHeader := c.Request.Header.Get(authKey)
+		if authHeader == "" {
+			c.Request.Header.Add(authKey, tokenPrefix+token)
+		}
 		c.Next()
 	}
 }
